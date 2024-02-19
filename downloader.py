5a8233227d996be3382ba79ba3eb69811265698c
agustin-Sanchez9 <agustinsanchezp123@gmail.com>
2024-02-19 18:38:48 -0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,11 +1,25 @@
 from pytube import YouTube
 
-print("")
-link = input("Paste link here: ")
-yt = YouTube(link)
-print("")
-print("Title: ", yt.title)
-print("Views: ", yt.views)
+
+correct_video = False
+
+while not correct_video:
+    
+    print("")
+    link = input("Paste link here: ")
+    yt = YouTube(link)
+    print("")
+
+    print("Title: ", yt.title)
+    print("Views: ", yt.author)
+
+    verification = input("Is this the video you like to download? (y/n) ")
+
+    if verification == "y":
+        correct_video = True
+    elif verification == "n":
+        pass
+        
 print("Wait for the download to finish")
 yd = yt.streams.get_highest_resolution()
 yd.download("C:/Users/Usuario/Desktop/mio/videos")
